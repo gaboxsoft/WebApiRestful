4d8e2853aeba0b161c484a8cca5852e5c15e1bca
gaboxsoft <gabox@msn.com>
2020-09-09 07:07:25 -0500

    Agregue tabla libros y actualice la BD"

---
+++ Entities/Libro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppBooks.Entities
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int AutorId { get; set; }
+
+
+    }
+}
