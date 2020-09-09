3d6d7605311856ccd4e7595dbd51c09272a8e59b
gaboxsoft <gabox@msn.com>
2020-09-09 07:30:29 -0500

    Adicione AutorController y metodo GET Async

---
+++ Controllers/AutorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppBooks.Contexts;
+using WebAppBooks.Entities;
+
+namespace WebAppBooks.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController]
+
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Autor>>> Get()
+        {
+            var autores =  await context.Autores.ToListAsync();
+            return autores;
+        }
+
+
+
+    }
+}
